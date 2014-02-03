d6e4fa27060ddb7a14a57f9dcef6bb4d5cf5c9a6
Traceback <traceback@nomsgs.com>
2014-02-03 16:52:32 -0500

    Leaf-only Tree

--- main.c
+++ main.c
@@ -4,20 +4,21 @@
 typedef struct treenode {
 	char id;
 	char isdata;
+	struct treenode *parent;
 	struct treenode *left;
 	struct treenode *right;
 } NODE;
 
 void destroy_tree(NODE *node);
-int add_node(NODE **node, char id);
+int add_node(NODE **node, char id, NODE *parent);
 void print_tree(NODE *node, int pad);
 
 int main() {
 	NODE *root = NULL;
 	int i;
 	
-	for(i = 0; i < 10; i++) {
-		add_node(&root, i);
+	for(i = 0; i < 3; i++) {
+		add_node(&root, i, NULL);
 	}
 	
 	print_tree(root, 0);
@@ -26,21 +27,35 @@ int main() {
 	return 0;
 }
 
-int add_node(NODE **node, char id) {
+int add_node(NODE **node, char id, NODE *parent) {
 	if(!(*node)) {
 		*node = malloc(sizeof(NODE));
 		(*node)->id = id;
 		(*node)->isdata = 1;
+		(*node)->parent = parent;
 		(*node)->left = NULL;
 		(*node)->right = NULL;
+		
+		if(parent && parent->isdata) {
+			parent->isdata = 0;
+			
+			if(!parent->left) {
+				add_node(&parent->left, parent->id, parent);
+			}
+			else {
+				add_node(&parent->right, parent->id, parent);
+			}
+			
+			parent->id = 0;
+		}
 		return 0;
 	}
 	else {
 		if(id > (*node)->id) {
-			return add_node(&(*node)->right, id);
+			return add_node(&(*node)->right, id, (*node));
 		}
 		else if(id < (*node)->id) {
-			return add_node(&(*node)->left, id);
+			return add_node(&(*node)->left, id, (*node));
 		}
 		else {
 			return 1;
